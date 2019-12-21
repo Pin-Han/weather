d5bea6a9f8446a601f7a682645764f74f99e5c52
rondo <patrick04184@gmail.com>
2019-12-22 00:37:18 +0800

    build weather descript

--- src/views/home.vue
+++ src/views/home.vue
@@ -1,52 +1,28 @@
 <template>
   <div>
     <div class="carousel">
-      <twmap class="carousel__map"></twmap>
+      <twmap class="carousel__map" @chooseCity="getweather"></twmap>
       <div class="carousel__img"></div>
-      <div class="carousel__search">
-        <select name="" id="" class="carousel__search-bar" v-model="chooseCity">
-          <option value="">請選擇城市</option>
-          <option :value="item" v-for="item in city" :key="item">{{
-            item
-          }}</option>
-        </select>
-        <button class="btn" id="search" @click="search()">
-          <i class="fas fa-search"></i>
-        </button>
-        <!-- <i class="fas fa-search carousel__search-icon"></i> -->
+      <div class="carousel__result">
+        <div class="carousel__city">
+          <p class="carousel__city-city">
+            {{ chooseCity }}
+          </p>
+          <p class="carousel__city-degree">
+            {{ result.average }}
+          </p>
+        </div>
+
+        <p class="carousel__result-descript">
+          {{ result.descript }}
+        </p>
+        <p class="carousel__result-descript" v-if="!result.descript ">
+          很抱歉，暫無{{ chooseCity }}的天氣資訊，請選取其他縣市取得天氣資訊。
+        </p>
       </div>
-      <!-- <div class="carousel__title">
+      <div class="carousel__title">
         <i class="fas fa-cloud-sun"></i>
         Taiwan weather
-        <!-- <i class="el-icon-cloudy-and-sunny"></i>
-        <i class="far fa-sun"></i> -->
-      <!-- </div> -->
-    </div>
-    <div class="weather" id="weather" v-if="chooseCity != ''">
-      <!-- <video autoplay muted loop class="weather__video">
-        <source :src="video_url" type="video/mp4" />
-        Sorry, your browser doesn't support embedded videos.
-      </video> -->
-      <img class="weather__img" :src="img_url" alt="tw_weather" />
-      <div class="info">
-        <div class="info__script">
-          <div class="info__sort">
-            <div class="info__sort-city">
-              {{ chooseCity }}
-            </div>
-            <div class="info__sort-degree">{{ result.average }}°</div>
-          </div>
-          <div class="info__script-description">
-            今天天氣，{{ result.descript }}
-          </div>
-        </div>
-        <div class="info__week">
-          <i class="fas fa-cloud-sun"></i>
-          <i class="fas fa-cloud-sun"></i>
-          <i class="fas fa-cloud-sun"></i>
-          <i class="fas fa-cloud-sun"></i>
-          <i class="fas fa-cloud-sun"></i>
-        </div>
       </div>
     </div>
   </div>
@@ -84,17 +60,6 @@ export default {
         "台東縣",
         "花蓮縣"
       ],
-      taiwan:[
-        {
-           'zh':"台北市",
-           'english':'taipei_city'
-        },
-        {
-          
-        }
-       
-
-      ],
       chooseCity: "",
       result: {
         average: "", //平均溫度
@@ -104,6 +69,7 @@ export default {
       date: "",
       phenomenon: "", //天氣現象
       img_url: "",
+      apiData: true, // api資料是否有
       weatherUrl: {
         rain:
           "https://images.unsplash.com/photo-1519692933481-e162a57d6721?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1500&q=80",
@@ -114,7 +80,9 @@ export default {
         wind:
           "https://images.unsplash.com/photo-1456356627738-3a96db6e2e33?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1358&q=80",
         sun:
-          "https://images.unsplash.com/photo-1525490829609-d166ddb58678?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1349&q=80"
+          "https://images.unsplash.com/photo-1525490829609-d166ddb58678?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1349&q=80",
+        normal:
+          "https://images.unsplash.com/photo-1519126400611-d57095205936?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80"
       }
     };
   },
@@ -122,6 +90,7 @@ export default {
     getweather(data) {
       //取得中央氣象局資料
       const vm = this;
+      vm.chooseCity = data;
       function promise() {
         return new Promise(function(resolve, reject) {
           axios
@@ -135,21 +104,25 @@ export default {
         });
       }
       promise().then(function(data) {
-        const nodata = data.data.records.locations[0].location.length;
-        const road = data.data.records.locations[0].location[0].weatherElement;
-        if (nodata === 0) {
-          alert("很抱歉");
-          return;
+
+        const nodata = data.data.records.locations[0].location;
+        console.log(nodata)
+        if (nodata.length === 0) {
+          vm.apiData = false;
+          vm.result = [];
+        } else {
+          vm.apiData = true;
+          const road =
+            data.data.records.locations[0].location[0].weatherElement;
+          //台北的天氣有問題
+          vm.result.chance = road[0].time[0].elementValue[0].value;
+          vm.result.average = `${road[1].time[0].elementValue[0].value}°`;
+          vm.result.descript = `今日天氣${road[10].time[0].elementValue[0].value}`;
+          vm.phenomenon = road[6].time[0].elementValue[0].value;
+          const today = new Date();
+          vm.date = `${today.getMonth()}/${today.getDate()}`;
+          vm.checkImg(vm.phenomenon);
         }
-        //台北的天氣有問題
-        vm.result.chance = road[0].time[0].elementValue[0].value;
-        vm.result.average = road[1].time[0].elementValue[0].value;
-        vm.result.descript = road[10].time[0].elementValue[0].value;
-        vm.phenomenon = road[6].time[0].elementValue[0].value;
-        const today = new Date();
-        vm.date = `${today.getMonth()}/${today.getDate()}`;
-        vm.checkImg(vm.phenomenon);
-        vm.loading = false;
       });
     },
     search() {
@@ -160,38 +133,29 @@ export default {
     checkImg(data) {
       //判斷天氣，顯示不同的圖片
       const vm = this;
-      const weatherImg = {
-        陰短暫雨: () => {
+      switch (data) {
+        case "陰短暫雨":
+        case "多雲短暫雨":
+        case "多雲時陰短暫雨":
           vm.img_url = vm.weatherUrl.rain;
-        },
-        多雲: () => {
+          break;
+        case "多雲":
+        case "多雲時晴":
+        case "多雲時陰":
           vm.img_url = vm.weatherUrl.cloud;
-        },
-        多雲時晴: () => {
-          vm.img_url = vm.weatherUrl.cloud;
-        },
-        陰天: () => {
+          break;
+        case "陰天":
           vm.img_url = vm.weatherUrl.cloudsDay;
-        },
-        晴時多雲: () => {
-          vm.img_url = vm.weatherUrl.cloud;
-        },
-        多雲短暫雨: () => {
-          vm.img_url = vm.weatherUrl.rain;
-        }
-      };
-      weatherImg[data]();
+        default:
+          vm.img_url = vm.weatherUrl.normal;
+      }
     }
   },
-  mounted() {
-    $("#search").click(function() {
-      $("html,body").animate(
-        {
-          scrollTop: $("#weather").offset().top
-        },
-        1000
-      );
-    });
-  }
+  computed: {
+    getdata() {
+      this.getweather(this.chooseCity);
+    }
+  },
+  mounted() {}
 };
 </script>
